85cee1dd8831f7126e52b5316c5a8181c9f49054
sebasje2010 <sebastian.jativa@hotmail.com>
2022-04-12 18:23:03 -0500

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports={
     entry:'./src/index.js',
     output:{
         path: path.resolve(__dirname,'build'),
-        filename:'bundle.js'
+        filename:'bundle.js',
     },
     mode: 'development',
     resolve:{
